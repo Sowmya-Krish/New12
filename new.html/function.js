8e6dde12fe8979504ff08cc3a5cdf6adcdb8f684
Sowmya-Krish <sowmikrish.crazy@gmail.com>
2022-09-20 09:36:22 +0530

    added html file

---
+++ new.html/function.js
@@ -0,0 +1,37 @@
+//function sum(num1, num2){
+ //   return num1 * num2
+//}
+//console.log(sum(5, 25));
+
+///let marks = [40, 90 , 80, 20, 30];
+//for (i = 0; i>marks.length; i++);
+//console.log("My mark is ", marks[i])
+
+//for (var mark of marks ){
+//    console.log("mark is",marks[i])
+//}
+
+
+//for (var index in m)
+//{
+ //   console.log("index is", index, is , m(index))
+//}
+
+
+
+//var student = {
+   // name : "sowmy",
+    //batch : "456",
+///};
+//console.log(student.name)
+//var marks = [40, 9, 10, 80];
+
+//var max = marks[0];
+//for( var mark of marks){
+   // if(mark > max)
+   // {
+     //   max = mark;
+  //  }
+//}
+//console.log(max)
+
---
+++ new.html/new1.js
@@ -0,0 +1,98 @@
+var x1 = 3;
+var x2 = "5";
+ 
+
+console.log(x1 + x2);
+
+console.log(x1 - x2);
+
+var t3 = "abc" - 3;
+
+console.log(t3)
+console.log(typeof t3);
+
+console.log("sowmi")
+var sowmi = "5";
+console.log(sowmi);
+
+let a = -5;
+let b = 2;
+
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a+"b");
+
+ console.log(typeof a);
+ console.log(typeof sowmi);
+
+const x = 5;
+const y = 6;
+const z = 7;
+console.log(x/y/z);
+
+var s = 2;
+var k = "2";
+
+console.log(s == k);
+
+console.log(s === k);
+
+var j = "10";
+var v = j;
+
+j = 20;
+
+console.log(j , v);
+
+var f = [1 , 2 , 3];
+var h = f;
+console.log(h[1]);
+
+var a1 = [1 , 2 , 3];
+var b1 = a1;
+b1[1] = 8;
+console.log(b1);
+
+
+const p = [2 , 8 , 15];
+Math.max(...p);
+Math.min(...p);
+console.log(Math.max(...p));
+console.log(Math.min(...p));
+
+const arr1 = [1 , 2 , 3];
+const arr2 = [...arr1 , 4 , 5 , 6];
+console.log(arr2);
+Math.max(...arr2);
+console.log(Math.max(...arr2));
+Math.min(...arr2);
+console.log(Math.min(...arr2));
+
+let ja = ["a", "b" , "c"];
+console.log(ja.length);
+console.log(ja);
+
+let va = ["1","2","3"];
+console.log(va);
+let check = va.splice(1, 0 , "4","5");
+console.log(check);
+
+var xy = myfunction (2 , 5);
+Function myfunction(a , b){
+   return a + b
+}
+console.log(xy);
+
+
+
+
+
+
+
+
+
+
+
+
+
